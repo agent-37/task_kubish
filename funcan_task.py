a38c50183ef369f9f05f59a501ec4327f21a969f
agent-37 <95122754+agent-37@users.noreply.github.com>
2024-11-20 22:15:16 +0300

    Update funcan_task.py

--- funcan_task.py
+++ funcan_task.py
@@ -1,89 +1,143 @@
+from cmath import sqrt
+from email.utils import collapse_rfc2231_value
+
 from PIL.ImageOps import expand
 from mpmath import zeros
-from sympy import Matrix, sin, integrate, simplify, expand, solve, diff
-from sympy.abc import x, y, l
+from sympy import Matrix, sin, integrate, simplify, expand, solve, diff, pretty, div, collect
+from sympy.abc import x, y, l, a
+
 # from cmath import sin,cos
-v1 = Matrix([2*sin(y), sin(3*y),y**2,y**3,y**4])
-mm = Matrix([[0.0,0.0,0.0,0.0,0.0],[0,0,0,0,0],[0,0,0,0,0],[0,0,0,0,0],[0,0,0,0,0]])
 
-bonus = 1
+all_root, all_func = [],[]
 
-for i in range(5):
-    for j in range(5):
-        if i==j:
-            mm[i,j]=(1-l*(integrate(v1[i]*v1[j],(y,0,1))))*bonus
-        else:
-            mm[i, j] = - l * (integrate(v1[i]*v1[j],(y,0,1)))*bonus
-print(mm)
-bb=0
-for i1 in range(5):
-    for i2 in range(5):
-        for i3 in range(5):
-            for i4 in range(5):
-                for i5 in range(5):
-                    qq={i1,i2,i3,i4,i5}
-                    if len(qq)==5:
-                        v=[i1,i2,i3,i4,i5]
-                        count=0
-                        for i in range(5):
-                            for j in range(5):
-                                if i<j and v[i]>v[j]:
-                                    count+=1
-                        # print((-1)**count,i1,i2,i3,i4,i5)
-                        bb+= (-1)**count*mm[0,i1]*mm[1,i2]*mm[2,i3]*mm[3,i4]*mm[4,i5]
-# print(mm)
-print(expand(bb))
-print(diff(expand(bb),l,l,l,l,l)/120)
-bb= expand(bb).evalf()
-# bb = bb- l**5*diff(expand(bb),l,l,l,l,l)/120
-print(bb)
-print(solve(bb))
-res_l = solve(bb)
+gamma = Matrix([sin(y), sin(3 * y), y ** 2, y ** 3, y ** 4])
+v2 = Matrix([4 * sin(y), sin(3 * y), y ** 2, y ** 3, y ** 4])
+for _ in range(5):
+    mm = Matrix([[0.0, 0.0, 0.0, 0.0, 0.0], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0]])
 
-for i in range(5):
-    for j in range(5):
-        mm[i,j]=mm[i,j].evalf()/bonus
-for root in res_l:
+    # rr = 0
+    # for i in range(5):
+    #     rr += v1[i]
+    #
+    # rr = expand(rr)
+    # print(rr)
+    # gl_int_sum = sqrt((integrate(expand(rr * rr), (y, 0, 1))).evalf()).real
+
+
+    for i in range(5):
+        for j in range(5):
+            if i == j:
+                mm[i, j] = (1 - l * (integrate(expand(gamma[j] * v2[i]), (y, 0, 1)))).evalf()
+            else:
+                mm[i, j] = - l * (integrate(expand(gamma[j] * v2[i]), (y, 0, 1))).evalf()
+    print(mm)
+
+
+    def det(mm):
+        bb = 0
+        for i1 in range(5):
+            for i2 in range(5):
+                for i3 in range(5):
+                    for i4 in range(5):
+                        for i5 in range(5):
+                            qq = {i1, i2, i3, i4, i5}
+                            if len(qq) == 5:
+                                v = [i1, i2, i3, i4, i5]
+                                count = 0
+                                for i in range(5):
+                                    for j in range(5):
+                                        if i < j and v[i] > v[j]:
+                                            count += 1
+                                # print((-1)**count,i1,i2,i3,i4,i5)
+                                bb += (-1) ** count * mm[0, i1] * mm[1, i2] * mm[2, i3] * mm[3, i4] * mm[4, i5]
+        return bb
+
+
+    bb = det(mm)
+    # print(mm)
+    print(expand(bb))
+    print(solve(bb))
+    res_l = solve(bb)
+    for i in range(len(res_l)):
+        for j in range(len(res_l)):
+            if abs(res_l[i])<abs(res_l[j]):
+                res_l[i],res_l[j]=res_l[j],res_l[i]
+    root = res_l[0]
+
     new_mm = mm.copy()
     for i in range(len(new_mm)):
-        new_mm[i]= new_mm[i].subs(l,root)
-    print(new_mm.det())
+        new_mm[i] = new_mm[i].subs(l, root)
     n = 5
-    bb=[]
+    bb = []
     for i1 in range(5):
-        bbb=[]
+        bbb = []
         for i2 in range(5):
-            bbb.append(new_mm[i1,i2])
+            bbb.append(new_mm[i1, i2])
         bb.append(bbb)
-    new_mm=bb
+    new_mm = bb.copy()
     # for i in new_mm:
     #     print(i)
     # print()
-    # for i in range(n):
-    #     if new_mm[i][i] == 0:
-    #         for j in range(i + 1, n):
-    #             if new_mm[j][i] != 0:
-    #                 new_mm[i], new_mm[j] = new_mm[j], new_mm[i]
-    #                 fl = 1
-    #                 break
-    #
-    #
-    #     bb = new_mm[i][i]
-    #     for j in range(i, n):
-    #         new_mm[i][j] /= bb
-    #
-    #
-    #     for j in range(i + 1, n):
-    #         if new_mm[j][i] != 0:
-    #             kk = new_mm[j][i]
-    #             for h in range(i, n):
-    #                 new_mm[j][h] -= kk * new_mm[i][h]
+
+    for i in range(n):
+        # if new_mm[i][i] == 0:
+        #     for j in range(i + 1, n):
+        #         if new_mm[j][i] != 0:
+        #             new_mm[i], new_mm[j] = new_mm[j], new_mm[i]
+        #             fl = 1
+        #             break
+
+        bb = new_mm[i][i]
+        for j in range(i, n):
+            new_mm[i][j] /= bb
+
+        for j in range(i + 1, n):
+            if new_mm[j][i] != 0:
+                kk = new_mm[j][i]
+                for h in range(i, n):
+                    new_mm[j][h] -= kk * new_mm[i][h]
     # for i in new_mm:
     #     print(i)
     # print()
-    # for i in range(n - 1, -1, -1):
-    #     for j in range(i - 1, -1, -1):
-    #         kk = new_mm[j][i]
-    #         new_mm[j][i] = 0
+    b = [0, 0, 0, 0, 1]
 
+    for i in range(n - 1, -1, -1):
+        for j in range(i - 1, -1, -1):
+            b[j] -= b[i] * new_mm[j][i]
+            new_mm[j][i] = 0
+    # print(b)
 
+    u = 0
+    for i in range(5):
+        u += b[i] * gamma[i]
+    # print(integrate(expand(u * u), (y, 0, 1)).evalf(),"!!!!!!!!!!!!!")
+    u /= sqrt(integrate(expand(u * u), (y, 0, 1)).evalf()).real
+
+    ww = 0
+    for i in range(5):
+        ww += gamma[i]* v2[i].subs(y, x)
+
+    print()
+    print(root)
+    print(u)
+    all_root.append(root)
+    all_func.append(u)
+    print(ww - u * u.subs(y, x) / root)
+    print(expand(ww - u * u.subs(y, x) / root))
+    www = expand(ww - u * u.subs(y, x) / root)
+    k_buf = www.copy()
+    for i in gamma:
+        k_buf = collect(k_buf,i)
+    for i in range(len(gamma)):
+        v2[i]= k_buf.coeff(gamma[i]).subs(x,y)
+    print(k_buf)
+    print(v2)
+    print('-------------------------------------------------------------------------------')
+for i in range(len(all_root)):
+    print(f'lambda_{i+1} = {all_root[i]}; func_{i+1} = {all_func[i].subs(y,x)}')
+f = sin(3*x) + a*x**2
+for i in range(len(all_root)):
+    # print())
+    bb  = integrate(expand(f * all_func[i].subs(y,x)), (x, 0, 1)).evalf()
+    print( bb, '///', (bb- a* bb.coeff(a)) / bb.coeff(a) )
+    # print(f'f_{i+1}={ sqrt(integrate(expand(f * all_func[i].subs(y,x)), (x, 0, 1)).evalf()).real}')
